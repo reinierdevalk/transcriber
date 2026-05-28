eac76e496e10432b37f7d326a4a5a478887516d0
Reinier de Valk <reinierdevalk@gmail.com>
2026-05-28 18:03:04 +0100

    typo in imports

--- py/diplomat.py
+++ py/diplomat.py
@@ -18,7 +18,7 @@ if lib_path not in sys.path:
 from py.constants import *
 from py.lxml_tools import (get_namespaces, collect_xml_ids, get_wrapper_elem, unwrap,
 						   make_element)
-from py.mei_tools import (get_main_MEI_elements, get_tuning, remove_empty_markup,  
+from py.mei_tools import (get_main_mei_elements, get_tuning, remove_empty_markup,  
 					  	  get_MEI_keysig, get_octave, get_midi_pitch, TUNINGS)
 from py.tools import add_unique_id, get_isodate
 
